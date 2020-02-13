e2fc397d4f1e054327c172adc08646ccf213e96c
fnhuang <felicia.hlj@gmail.com>
2020-02-13 18:34:25 +0800

    chromedriver

--- scraper.py
+++ scraper.py
@@ -16,7 +16,8 @@ class ReviewCrawler():
         options.add_argument("--disable-notifications")
         # options.add_argument("--headless")
 
-        driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
+        #driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
+        driver = webdriver.Chrome()
         # driver = webdriver.PhantomJS("C:\Program Files (x86)\Google\Chrome\phantomjs")
         driver.get(url)
 
